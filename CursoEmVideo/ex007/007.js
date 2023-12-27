e00c02b6769713ebbdbe8a89815f7b80ce487654
Caio Sena <133704390+Caiosenaf15@users.noreply.github.com>
2023-12-26 21:34:37 -0300

    Estudos

---
+++ CursoEmVideo/ex007/007.js
@@ -0,0 +1,6 @@
+var vel = 81
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel >= 80) { // Condição simples
+    console.log(`Você ultrapassou a velocidade máxima!`)
+}
+console.log(`Dirija com segurança!`)
